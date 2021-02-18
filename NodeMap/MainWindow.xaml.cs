7a144e16a9af2c5e9d060d08489fbd64986bb55e
bruno chappe <bruno.chappe@gmail.com>
2021-02-18 17:52:42 +0100

    Initial commit

---
+++ NodeMap/MainWindow.xaml.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace NodeMap
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Vector2 start;
+        private Vector2 end;
+        private List<Vector2> forbidenNodes = new List<Vector2>();
+        private List<Vector2> map;
+        private Point currentPointer;
+        private Point endPointer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            NodesMap.MouseLeftButtonDown += NodesMap_MouseLeftButtonDown;
+            NodesMap.MouseLeftButtonUp += NodesMap_MouseLeftButtonUp;
+            forbidenNodes = new List<Vector2>();
+        }
+
+        private List<Rect> obstacles = new List<Rect>();
+
+
+
+        private void NodesMap_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                endPointer = Mouse.GetPosition(NodesMap);
+
+
+                obstacles.Add(new Rect(currentPointer, endPointer));
+                var newObstacles = map.Where(v =>
+                  {
+
+                      return v.X > currentPointer.X && v.X < endPointer.X
+                                                    && v.Y > currentPointer.Y && v.Y < endPointer.Y;
+
+
+                  }).ToList();
+
+                foreach (var obstacle in newObstacles)
+                {
+                    var visual = NodesMap.Children.OfType<Ellipse>().FirstOrDefault(el => el.Tag is Vector2 v && v == obstacle);
+                    if (visual != null)
+                    {
+                        visual.Fill = new SolidColorBrush(Colors.Red);
+                    }
+                }
+                forbidenNodes.AddRange(newObstacles);
+
+            }
+
+            if (Keyboard.Modifiers == ModifierKeys.Shift)
+            {
+                var startClick = Mouse.GetPosition(NodesMap);
+                inputStartX.Text = startClick.X.ToString();
+                inputStartY.Text = startClick.Y.ToString();
+                start = new Vector2((float)startClick.X, (float)startClick.Y);
+
+            }
+
+            if (Keyboard.Modifiers == ModifierKeys.Alt)
+            {
+                var endClick = Mouse.GetPosition(NodesMap);
+                inputEndX.Text = endClick.X.ToString();
+                inputEndY.Text = endClick.Y.ToString();
+
+                end = new Vector2((float)endClick.X, (float)endClick.Y);
+
+            }
+        }
+
+        private void NodesMap_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                currentPointer = Mouse.GetPosition(NodesMap);
+            }
+
+        }
+
+        private void El_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            FrameworkElement fe = (FrameworkElement)sender;
+            nodeDisplay.Text = new Point(Canvas.GetLeft(fe) + fe.Width / 2, Canvas.GetTop(fe) + fe.Height / 2).ToString();
+        }
+
+        private void RangeBase_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            //if (NodesMap != null)
+            //{
+            //    NodesMap.RenderTransformOrigin = new Point(0.5, 0.5);
+            //    NodesMap.RenderTransform = new ScaleTransform(zoom.Value, zoom.Value);
+            //}
+        }
+
+        private void Generate_OnClick(object sender, RoutedEventArgs e)
+        {
+            obstacles.Clear();
+            forbidenNodes.Clear();
+            NodesMap.Children.Clear();
+
+            start = new Vector2((float)Convert.ToDouble(inputStartX.Text), (float)Convert.ToDouble(inputStartY.Text));
+            end = new Vector2((float)Convert.ToDouble(inputEndX.Text), (float)Convert.ToDouble(inputEndY.Text)); ;
+
+            map = new List<Vector2>();
+            float ndSpaceing = (float)Convert.ToDouble(inputNodeSpacing.Text);
+
+            float offset = (checkOffsetNodes.IsChecked.HasValue && checkOffsetNodes.IsChecked.Value ? ndSpaceing / 2 : 0.0f);
+
+            for (float i = 0; i < NodesMap.ActualWidth; i += ndSpaceing)
+            {
+                bool applyOffset = false;
+                for (float j = 0; j < NodesMap.ActualHeight; j += ndSpaceing)
+                {
+                    Vector2 node;
+                    if (!applyOffset)
+                    {
+                        node = new Vector2(i, j);
+                        applyOffset = true;
+                    }
+                    else
+                    {
+                        node = new Vector2(i + offset, j);
+                        applyOffset = false;
+                    }
+                    map.Add(node);
+                }
+            }
+
+            bool startAlreadyInMap = false;
+            bool endAlreadyInMap = false;
+            foreach (var vector2 in map)
+            {
+                Ellipse el = new Ellipse() { Height = 5, Width = 5, Tag = vector2 };
+                if (vector2 == start)
+                {
+                    startAlreadyInMap = true;
+                    el.Fill = new SolidColorBrush(Colors.Green);
+                }
+                else if (vector2 == end)
+                {
+                    endAlreadyInMap = true;
+                    el.Fill = new SolidColorBrush(Colors.Yellow);
+                }
+
+                else if (forbidenNodes.Contains(vector2))
+                {
+                    el.Fill = new SolidColorBrush(Colors.Red);
+                }
+                else
+                {
+                    el.Fill = new SolidColorBrush(Colors.DarkGray);
+                }
+
+
+                NodesMap.Children.Add(el);
+
+
+
+
+
+                Canvas.SetTop(el, vector2.Y - el.Height / 2);
+                Canvas.SetLeft(el, vector2.X - el.Width / 2);
+
+                el.MouseEnter += El_MouseEnter;
+            }
+
+            if (!startAlreadyInMap)
+            {
+                map.Add(start);
+                Ellipse el = new Ellipse() { Height = 5, Width = 5, Fill = new SolidColorBrush(Colors.Green), Tag = start };
+                NodesMap.Children.Add(el);
+                Canvas.SetTop(el, start.Y - el.Height / 2);
+                Canvas.SetLeft(el, start.X - el.Width / 2);
+            }
+
+            if (!endAlreadyInMap)
+            {
+                map.Add(end);
+                Ellipse el = new Ellipse() { Height = 5, Width = 5, Fill = new SolidColorBrush(Colors.DarkRed), Tag = end };
+                NodesMap.Children.Add(el);
+                Canvas.SetTop(el, end.Y - el.Height / 2);
+                Canvas.SetLeft(el, end.X - el.Width / 2);
+            }
+        }
+
+        class CanvasProgressDisplay : IPathProgressDisplayer
+        {
+            private readonly Canvas _nodeContainer;
+            private Ellipse _previousCurrent;
+            private List<Ellipse> _previousCandidates;
+
+            public CanvasProgressDisplay(Canvas nodeContainer)
+            {
+                _nodeContainer = nodeContainer;
+            }
+            public async Task NewClosed(CandidateNode current)
+            {
+
+                await Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+                 {
+                     //if (_previousCurrent != null)
+                     //{
+                     //    _previousCurrent.Fill = new SolidColorBrush(Colors.DarkGray);
+                     //}
+
+                     var visual = _nodeContainer.Children.OfType<Ellipse>().FirstOrDefault(el => el.Tag is Vector2 v && v == current.Position);
+                     if (visual != null)
+                     {
+                         visual.Fill = new SolidColorBrush(Colors.Purple);
+                         _previousCurrent = visual;
+
+                     }
+                     Thread.Sleep(50);
+                 }));
+            }
+
+            public async Task NewCandidates(List<CandidateNode> adjacentNodes)
+            {
+                await Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+                  {
+                      if (_previousCandidates == null)
+                          _previousCandidates = new List<Ellipse>();
+
+                      //if (_previousCandidates != null && _previousCandidates.Any())
+                      //{
+                      //    foreach (var previousCandidate in _previousCandidates)
+                      //    {
+                      //        previousCandidate.Fill = new SolidColorBrush(Colors.DarkGray);
+                      //    }
+                      //    _previousCandidates.Clear();
+                      //}
+                      foreach (var adjnode in adjacentNodes)
+                      {
+                          var visual = _nodeContainer.Children.OfType<Ellipse>().FirstOrDefault(el => el.Tag is Vector2 v && v == adjnode.Position);
+                          if (visual != null)
+                          {
+                              visual.Fill = new SolidColorBrush(Colors.Lime);
+                              _previousCandidates.Add(visual);
+
+                          }
+                      }
+                      Thread.Sleep(50);
+
+                  }));
+            }
+        }
+        private async void FindPath_OnClick(object sender, RoutedEventArgs e)
+        {
+            Stopwatch sw = new Stopwatch();
+            _pf = new PathFinder();
+            var proxRad = (float)Convert.ToDouble(inputProximity.Text);
+
+            //Shuffle(map);
+            ClearNodes();
+
+            IAStarHeuristic heur = null;
+            if (h_no.IsChecked.HasValue && h_no.IsChecked.Value)
+            {
+                heur = new NoHeuristic();
+            }
+            if (h_ad.IsChecked.HasValue && h_ad.IsChecked.Value)
+            {
+                heur = new CurrentToEndAbsoluteDistance();
+            }
+            if (h_adls.IsChecked.HasValue && h_adls.IsChecked.Value)
+            {
+                heur = new CurrentToEndAbsoluteDistanceWithLineOfSight((float) Convert.ToDouble(obstModifierInput.Text));
+            }
+            if (h_adlsc.IsChecked.HasValue && h_adlsc.IsChecked.Value)
+            {
+                heur = new CurrentToEndAbsoluteDistanceWithRedundency((float)Convert.ToDouble(obstModifierInput.Text));
+            }
+
+            var finalPath = await Task.Run(async () =>
+            {
+                sw.Reset();
+                _pf.Init(start, end, map, proxRad, forbidenNodes, obstacles, heur);
+                sw.Start();
+                var result = await _pf.Start(new CanvasProgressDisplay(NodesMap));
+                sw.Stop();
+                return result;
+            });
+            displayfindPathTime.Text = sw.Elapsed.TotalMilliseconds.ToString() + "ms";
+
+            sw.Reset();
+            sw.Start();
+            List<Tuple<Vector2, Vector2>> meshes = new List<Tuple<Vector2, Vector2>>();
+            for (int i = 0; i < finalPath.Count; i++)
+            {
+                if (i + 1 < finalPath.Count)
+                {
+                    meshes.Add(new Tuple<Vector2, Vector2>(finalPath[i].Position, finalPath[i + 1].Position));
+                }
+            }
+
+            foreach (var tuple in meshes)
+            {
+                Line line = new Line
+                {
+                    X1 = tuple.Item1.X,
+                    X2 = tuple.Item2.X,
+                    Y1 = tuple.Item1.Y,
+                    Y2 = tuple.Item2.Y,
+                    Stroke = new SolidColorBrush(Colors.CornflowerBlue),
+                    StrokeThickness = 1
+                };
+                NodesMap.Children.Add(line);
+            }
+            sw.Stop();
+            Console.WriteLine("Drawing meshes time : " + sw.Elapsed.TotalMilliseconds + "ms");
+
+        }
+
+        private void ClearNodes()
+        {
+            foreach (var node in NodesMap.Children.OfType<Ellipse>())
+            {
+                if (node.Tag is Vector2 v1 && v1 == start)
+                    continue;
+                if (node.Tag is Vector2 v2 && v2 == end)
+                    continue;
+                if (node.Tag is Vector2 v3 && forbidenNodes.Contains(v3))
+                    continue;
+
+                node.Fill = new SolidColorBrush(Colors.DarkGray);
+            }
+        }
+
+        private static Random rng = new Random();
+        private PathFinder _pf;
+
+        public static void Shuffle<T>(IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+        private void StopPath_OnClick(object sender, RoutedEventArgs e)
+        {
+            _pf.Stop();
+        }
+    }
+
+    public interface IPathProgressDisplayer
+    {
+        Task NewClosed(CandidateNode current);
+        Task NewCandidates(List<CandidateNode> adjacentNodes);
+    }
+}
---
+++ NodeMap/PathFinder.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace NodeMap
+{
+    public interface IAStarHeuristic
+    {
+        float Compute(Vector2 start, Vector2 end, List<Vector2> forbiden, List<Rect> obstacles,
+            Dictionary<Vector2, int> redundency);
+    }
+
+    public class NoHeuristic : IAStarHeuristic
+    {
+        public float Compute(Vector2 start, Vector2 end, List<Vector2> forbiden, List<Rect> obstacles,
+            Dictionary<Vector2, int> redundency)
+        {
+            return 0;
+        }
+    }
+
+    public class CurrentToEndAbsoluteDistance : IAStarHeuristic
+    {
+        public float Compute(Vector2 start, Vector2 end, List<Vector2> forbiden, List<Rect> obstacles,
+            Dictionary<Vector2, int> redundency)
+        {
+            return Math.Abs(end.X - start.X) + Math.Abs(end.Y - start.Y);
+        }
+    }
+    public class CurrentToEndAbsoluteDistanceWithRedundency : IAStarHeuristic
+    {
+        private readonly float _modifier;
+
+        public CurrentToEndAbsoluteDistanceWithRedundency(float modifier)
+        {
+            _modifier = modifier;
+        }
+        public float Compute(Vector2 start, Vector2 end, List<Vector2> forbiden, List<Rect> obstacles,
+            Dictionary<Vector2, int> redundency)
+        {
+            float intersectModifier = 0.0f;
+            if (redundency.TryGetValue(start, out var red))
+            {
+                intersectModifier = (red-1) * _modifier;
+            }
+            //foreach (var obstacle in obstacles)
+            //{
+            //    bool intersect = IntersectHelpers.LineIntersectsRect(new Point(start.X, start.Y), new Point(end.X, end.Y), obstacle);
+            //    if (intersect)
+            //        intersectModifier += _modifier;
+            //}
+            return Math.Abs(end.X - start.X) + Math.Abs(end.Y - start.Y) + intersectModifier;
+        }
+    }
+    public class CurrentToEndAbsoluteDistanceWithLineOfSight : IAStarHeuristic
+    {
+        private readonly float _modifier;
+
+        public CurrentToEndAbsoluteDistanceWithLineOfSight(float modifier)
+        {
+            _modifier = modifier;
+        }
+        public float Compute(Vector2 start, Vector2 end, List<Vector2> forbiden, List<Rect> obstacles,
+            Dictionary<Vector2, int> redundency)
+        {
+            float intersectModifier = 0;
+            foreach (var obstacle in obstacles)
+            {
+                bool intersect = IntersectHelpers.LineIntersectsRect(new Point(start.X, start.Y), new Point(end.X, end.Y), obstacle);
+                if (intersect)
+                    intersectModifier += _modifier;
+            }
+            return Math.Abs(end.X - start.X) + Math.Abs(end.Y - start.Y) + intersectModifier;
+        }
+    }
+    public class PathFinder
+    {
+        private static CandidateNode _current;
+        private static CandidateNode _startLoc;
+        private static Vector2 _targetLoc;
+        private static List<CandidateNode> _openList;
+        private static List<CandidateNode> _closedList;
+        private static int _g;
+        private static float _proximityRadius = 20;
+        /*private PathRenderer Renderer;
+    private List<PathRenderer> pathRenderers;*/
+        private List<Vector2> _instanceValidMapNodes;
+        private List<Vector2> _forbidenNodes;
+        private IAStarHeuristic _heuristic;
+        private List<Rect> _obstacles;
+        private bool _stopped;
+        private Dictionary<Vector2, int> _redundency;
+
+        public void Init(Vector2 start, Vector2 destination, List<Vector2> instanceValidMapNodes, float proximityRadius,
+            List<Vector2> forbidenNodes,
+            List<Rect> obstacles, IAStarHeuristic heuristic)
+        {
+            _heuristic = heuristic;
+            _obstacles = obstacles;
+            _forbidenNodes = forbidenNodes;
+            _proximityRadius = proximityRadius;
+            _instanceValidMapNodes = instanceValidMapNodes;
+            /*pathRenderers?.Clear();
+        pathRenderers = new List<PathRenderer>();
+        Renderer = new PathRenderer();*/
+            _current = null;
+            _startLoc = new CandidateNode() { Position = start };
+            _targetLoc = destination;
+            _openList = new List<CandidateNode>();
+            _closedList = new List<CandidateNode>();
+            _g = 0;
+            _openList.Add(_startLoc);
+            _redundency = new Dictionary<Vector2, int>();
+        }
+
+        public async Task<List<CandidateNode>> Start(IPathProgressDisplayer display)
+        {
+            while (_openList.Count > 0 && !_stopped)
+            {
+                // algorithm's logic goes here
+                // get the square with the lowest F score
+                var lowest = _openList.Min(l => l.F_Score);
+                _current = _openList.First(l => Math.Abs(l.F_Score - lowest) < 0.05);
+
+                // add the current square to the closed list
+                _closedList.Add(_current);
+                await display.NewClosed(_current);
+
+
+
+                // remove it from the open list
+                _openList.Remove(_current);
+
+                //if (closedList.FirstOrDefault(l => l.Position.x == targetLoc.x && l.Position.y == targetLoc.y) != null) //Original
+                if (_closedList.FirstOrDefault(l => Math.Abs(l.Position.X - _targetLoc.X) < 0.05 && Math.Abs(l.Position.Y - _targetLoc.Y) < 0.05) != null)
+                    break;
+
+                var adjacentNodes = GetWalkableAdjacentNodes(_current.Position, _instanceValidMapNodes);
+                _g++;
+                await display.NewCandidates(adjacentNodes);
+
+                foreach (var adjacentSquare in adjacentNodes)
+                {
+                    if (_redundency.ContainsKey(adjacentSquare.Position))
+                    {
+                        _redundency[adjacentSquare.Position] += 1;
+                    }
+                    else
+                    {
+                        _redundency.Add(adjacentSquare.Position, 1);
+                    }
+                    // if this adjacent square is already in the closed list, ignore it
+                    var alreadyInClosedList = _closedList.FirstOrDefault(l => l.Position == adjacentSquare.Position);
+                    if (alreadyInClosedList != null)
+                    {
+
+
+                        continue;
+                    }
+
+                    // if it's not in the open list...
+                    if (_openList.FirstOrDefault(l => l.Position == adjacentSquare.Position) == null)
+                    {
+                        // compute its score, set the parent
+                        adjacentSquare.G_Score = _g;
+                        adjacentSquare.H_Score = _heuristic.Compute(adjacentSquare.Position, _targetLoc, _forbidenNodes, _obstacles, _redundency);
+                        adjacentSquare.F_Score = adjacentSquare.G_Score + adjacentSquare.H_Score;
+                        adjacentSquare.Parent = _current;
+
+                        // and add it to the open list
+                        _openList.Insert(0, adjacentSquare);
+                        /*PathRenderer pathDisplay = new PathRenderer();
+                    pathDisplay.DrawGoalLine(current.Position, adjacentSquare.Position);
+                    pathRenderers.Add(pathDisplay);*/
+                    }
+                    else
+                    {
+                        // test if using the current G score makes the adjacent square's F score
+                        // lower, if yes update the parent because it means it's a better path
+                        if (_g + adjacentSquare.H_Score < adjacentSquare.F_Score)
+                        {
+                            adjacentSquare.G_Score = _g;
+                            adjacentSquare.F_Score = adjacentSquare.G_Score + adjacentSquare.H_Score;
+                            adjacentSquare.Parent = _current;
+                        }
+                    }
+                }
+
+            }
+
+            List<CandidateNode> finalPath = new List<CandidateNode>();
+            CandidateNode temp = _closedList[_closedList.IndexOf(_current)];
+            if (temp == null) return null;
+            do
+            {
+                finalPath.Add(temp);
+                temp = temp.Parent;
+            } while (temp != _startLoc && temp != null);
+            return finalPath;
+
+            //_closedList.Reverse();
+            //return _closedList;
+        }
+        List<CandidateNode> GetWalkableAdjacentNodes(Vector2 currentLoc, List<Vector2> map)
+        {
+            //draw proximity area around current position
+            /*Bounds proximityBounds = new Bounds(new Vector3(currentLoc.x - _proximityRadius, currentLoc.y - _proximityRadius, 0), new Vector3(_proximityRadius * 2, _proximityRadius * 2, 0));*/
+
+            return
+                //get map nodes which falls inside the proximity bounds and are not forbidden nodes (obstacles)
+                map
+                    .Where(p => !_forbidenNodes.Contains(p))
+                    .Where(mapNode => IsInBound(currentLoc, mapNode))
+                    //create walkable candidate Node
+                    .Select(node => new CandidateNode() { Position = new Vector2(node.X, node.Y) })
+                    .ToList();
+        }
+
+        bool IsInBound(Vector2 currentPoint, Vector2 targetPoint)
+        {
+            return IntersectHelpers.IsPointInRect(targetPoint, currentPoint, _proximityRadius * 2, _proximityRadius * 2);
+            //float left = currentPoint.X - _proximityRadius;
+            //float right = left + _proximityRadius * 2;
+            //float top = currentPoint.Y - _proximityRadius;
+            //float bottom = top + _proximityRadius * 2;
+
+            //return targetPoint.X >= left
+            //       && targetPoint.X <= right
+            //       && targetPoint.Y >= top
+            //       && targetPoint.Y <= bottom;
+
+
+        }
+
+        private float ComputeHScore(Vector2 current, Vector2 destination)
+        {
+
+            return 0;
+        }
+
+        public void Stop()
+        {
+            _stopped = true;
+        }
+    }
+
+    public class IntersectHelpers
+    {
+        public static bool IsPointInRect(Vector2 target, Vector2 rectPosition, float widthRadius, float heightRadius)
+        {
+            var rect = new Rect(new Point(rectPosition.X - widthRadius, rectPosition.Y - heightRadius), new Size(widthRadius * 2, heightRadius * 2));
+            return rect.Contains(target.X, target.Y);
+        }
+        public static bool LineIntersectsRect(Point p1, Point p2, Rect r)
+        {
+            return LineIntersectsLine(p1, p2, new Point(r.X, r.Y), new Point(r.X + r.Width, r.Y)) ||
+                   LineIntersectsLine(p1, p2, new Point(r.X + r.Width, r.Y), new Point(r.X + r.Width, r.Y + r.Height)) ||
+                   LineIntersectsLine(p1, p2, new Point(r.X + r.Width, r.Y + r.Height), new Point(r.X, r.Y + r.Height)) ||
+                   LineIntersectsLine(p1, p2, new Point(r.X, r.Y + r.Height), new Point(r.X, r.Y)) ||
+                   (r.Contains(p1) && r.Contains(p2));
+        }
+
+        private static bool LineIntersectsLine(Point l1p1, Point l1p2, Point l2p1, Point l2p2)
+        {
+            float q = (float)((l1p1.Y - l2p1.Y) * (l2p2.X - l2p1.X) - (l1p1.X - l2p1.X) * (l2p2.Y - l2p1.Y));
+            float d = (float)((l1p2.X - l1p1.X) * (l2p2.Y - l2p1.Y) - (l1p2.Y - l1p1.Y) * (l2p2.X - l2p1.X));
+
+            if (d == 0)
+            {
+                return false;
+            }
+
+            float r = q / d;
+
+            q = (float)((l1p1.Y - l2p1.Y) * (l1p2.X - l1p1.X) - (l1p1.X - l2p1.X) * (l1p2.Y - l1p1.Y));
+            float s = q / d;
+
+            if (r < 0 || r > 1 || s < 0 || s > 1)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+
+
+}
